b8aac177ef3320e8ba52d4834a889879a05eee08
Agureeva Anna <agureeva@ro.ru>
2022-12-12 17:26:04 +0300

    Init commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,65 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    protected Map<String, ArrayList<PageEntry>> totalWords;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+
+        totalWords = new HashMap<>();
+
+        Map<String, Integer> freqs = new HashMap<>();
+        List<File> pdfFileList = List.of(pdfsDir.listFiles());
+
+        for (File pdf : pdfFileList) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+
+            for (int nomberOfPage = 1; nomberOfPage <= doc.getNumberOfPages(); nomberOfPage++) {
+                var text = PdfTextExtractor.getTextFromPage(doc.getPage(nomberOfPage));
+                var words = text.split("\\P{IsAlphabetic}+");
+
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+                int count;
+                for (var j : freqs.keySet()) {
+                    String word = j.toLowerCase();
+                    if (freqs.get(word) != null) {
+                        count = freqs.get(word.toLowerCase());
+
+                        totalWords.computeIfAbsent(word, f -> new ArrayList<PageEntry>()).add(new PageEntry(pdf.getName(), nomberOfPage, count));
+
+                    }
+                    Collections.sort(totalWords.get(word));
+                }
+                freqs.clear();
+
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        List<PageEntry> searchResult = new ArrayList<>();
+        if (totalWords.get(word.toLowerCase()) != null) {
+            searchResult = totalWords.get(word.toLowerCase());
+        }
+
+        return searchResult;
+    }
+
+    @Override
+    public String toString() {
+        return "BooleanSearchEngine{" + "totalWords=" + totalWords + '}';
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,52 @@
 public class PageEntry implements Comparable<PageEntry> {
-    private final String pdfName;
+    private final String fileName;
     private final int page;
     private final int count;
 
-    // ???
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public PageEntry(String fileName, int page, int count) {
+        this.fileName = fileName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry p) {
+        if (count < p.count) {
+            return 1;
+        } else if (count > p.count) {
+            return -1;
+        } else {
+            if (page < p.page) {
+                return 1;
+            } else if (page > p.page) {
+                return -1;
+            } else {
+                return getFileName().compareTo(p.getFileName());
+            }
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "pdfName='" + fileName + '\'' +
+                ", page=" + page +
+                ", count=" + count +
+                '}';
+    }
+
 }
